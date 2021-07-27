3abdf5fef29f36ceb30752abda6545f546453c21
Santiago <smferro@gmail.com>
2021-07-27 17:19:05 -0300

    deployyyy

--- api/index.js
+++ api/index.js
@@ -21,11 +21,12 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const { Diet } = require('./src/db.js');
 const { diets } = require('./constants');
+const PORT = process.env.PORT || 3001
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log(`%s listening at ${process.env.PORT}`); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
 
     diets.map(d => {
       Diet.create({ name: d })
